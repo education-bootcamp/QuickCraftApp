b39d0babb313c8284af78689242eb10883704f7c
education-bootcamp <education.seekerscloud@gmail.com>
2025-03-17 21:06:40 +0530

    4

--- app/navigation/tab-navigation/HomeBottomTabNavigation.tsx
+++ app/navigation/tab-navigation/HomeBottomTabNavigation.tsx
@@ -1,3 +1,4 @@
+import {Image,Text, TouchableOpacity} from 'react-native';
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import HomeMenuScreen from "@/components/ui/screen/home/HomeMenuScreen";
 import HomeCartScreen from "@/components/ui/screen/home/HomeCartScreen";
@@ -6,7 +7,7 @@ import HomeBookmarkScreen from "@/components/ui/screen/home/HomeBookmarkScreen";
 import HomeProductsScreen from "@/components/ui/screen/home/HomeProductsScreen";
 import {Ionicons} from "@expo/vector-icons";
 import {COLORS} from "@/constants/CollorPallet";
-
+const logo = require('../../../assets/images/logo/logo-wattpad.png');
 const Tab = createBottomTabNavigator();
 export default function HomeBottomTabNavigation({navigation}:any){
     return(
@@ -29,7 +30,33 @@ export default function HomeBottomTabNavigation({navigation}:any){
         >
             <Tab.Screen name={'Menu'} component={HomeMenuScreen}/>
             <Tab.Screen name={'Cart'} component={HomeCartScreen}/>
-            <Tab.Screen name={'Home'} component={HomePageScreen}/>
+            <Tab.Screen name={'Home'}
+                        component={HomePageScreen}
+                        options={{
+                            headerLeft:()=>(
+                                <Image source={logo} resizeMode={'contain'}
+                                style={{width:130, height:80, marginLeft:10}}
+                                />
+                            ),
+                            headerTitle:'',
+                            headerRight:()=>(
+                                <TouchableOpacity
+                                style={{
+                                    marginRight:10,
+                                    width:120,
+                                    backgroundColor:COLORS.orange,
+                                    borderRadius:3,
+                                    height:35,
+                                    alignItems:'center',
+                                    justifyContent:'center'
+                                }}
+                                >
+                                    <Text style={{color:COLORS.light}}>Admin Console</Text>
+                                </TouchableOpacity>
+                            )
+                        }}
+
+            />
             <Tab.Screen name={'Bookmarks'} component={HomeBookmarkScreen}/>
             <Tab.Screen name={'Products'} component={HomeProductsScreen}/>
         </Tab.Navigator>
