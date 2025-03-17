010282333a89a9c0a6340f6b95d2c45084bada72
education-bootcamp <education.seekerscloud@gmail.com>
2025-03-17 21:18:33 +0530

    5

--- components/ui/screen/home/HomeMenuScreen.tsx
+++ components/ui/screen/home/HomeMenuScreen.tsx
@@ -1,15 +1,24 @@
 import {View, Text, StyleSheet} from 'react-native';
+import { Menu } from 'react-native-paper';
+import {COLORS} from "@/constants/CollorPallet";
 export default function HomeMenuScreen(){
     return(
         <View style={styles.container}>
-            <Text>Home Menu Screen</Text>
+            <Menu.Item leadingIcon="redo" onPress={() => {}} title="My Orders" />
+            <Menu.Item leadingIcon="undo" onPress={() => {}} title="Settings" />
+            <Menu.Item leadingIcon="content-cut" onPress={() => {}} title="Vouchers" />
+            <Menu.Item leadingIcon="content-copy" onPress={() => {}} title="Daily Deals" />
+            <Menu.Item leadingIcon="content-paste" onPress={() => {}} title="Notifications" />
+            <Menu.Item leadingIcon="content-paste" onPress={() => {}} title="Latest Products" />
+            <Menu.Item leadingIcon="content-paste" onPress={() => {}} title="Profile" />
+            <Menu.Item leadingIcon="content-paste" onPress={() => {}} title="Customer Care" />
+            <Menu.Item leadingIcon="content-paste" onPress={() => {}} title="Logout" />
         </View>
     )
 }
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        alignItems:'center',
-        justifyContent:'center'
+        backgroundColor:COLORS.light
     }
 })
